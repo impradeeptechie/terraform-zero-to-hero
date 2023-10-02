c9d470d24ad98b736f0db3d04e46021bc7f880fd
Pradeep Raj <91276885+impradeeptechie@users.noreply.github.com>
2023-10-03 01:14:42 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-08f32efd140b7d89f"  # Specify an appropriate AMI ID
+    ami           = "ami-01dd271720c1ba44f"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-0f433ea586e3ad02c"
     key_name = "aws_login"
